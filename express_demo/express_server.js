bd2df7681f6a182df541d787a87552bc3d9c5d46
Ted <kimtx2017@gmail.com>
2017-08-13 22:30:46 +0900

    Fix bug

--- express_demo/express_server.js
+++ express_demo/express_server.js
@@ -3,7 +3,12 @@ var app = express();
 var http = require('http');
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
-function searchPubTransPathAJAX(res, apiKey, sx, sy, ex, ey) {
+function searchPubTransPathAJAX(req, res, apiKey) {
+    
+    var sx = req.query.SX
+    var sy = req.query.SY;
+    var ex = req.query.EX
+    var ey = req.query.EY;
     
     var xhr = new XMLHttpRequest();
     var url = 'https://api.odsay.com/api/searchPubTransPath?SX='+sx+'&SY='+sy+'&EX='+ex+'&EY='+ey+'&apiKey='+apiKey;
@@ -26,18 +31,13 @@ function searchPubTransPathAJAX(res, apiKey, sx, sy, ex, ey) {
 
 app.get('/route', function(req, res){
     
-    var sx = req.query.SX
-    var sy = req.query.SY;
-    var ex = req.query.EX
-    var ey = req.query.EY;
-    
     console.log('SX = '+sx+' / SY = '+sy);
     console.log('EX = '+ex+' / EY = '+ey);
     
     var apiKey = 'bFNbGwjMxjtTaiMEroskKw';
     
     console.log('start request');
-    searchPubTransPathAJAX(res, apiKey, sx, sy, ex, ey);
+    searchPubTransPathAJAX(req, res, apiKey);
 });
 
 // 서버 활성화 설정해 놓은 port 와 hostName 의 주소로 들어오는 클라이언트들을 감지한다.
