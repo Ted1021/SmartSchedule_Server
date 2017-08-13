c4d67c7a8e1cc83f4aea2b58d5f986f9ebc14541
Ted <kimtx2017@gmail.com>
2017-08-13 16:56:04 +0900

    Correct API logic

--- express_demo/express_server.js
+++ express_demo/express_server.js
@@ -108,17 +108,15 @@ app.get('/path', function(req, res){
 
 		  if (xhr.readyState == 4 && xhr.status == 200) {
               console.log(">>>>>>>>> return from Odsay");
-			 console.log( xhr.responseText ); // <- xhr.responseText 로 결과를 가져올 수 있음
+              console.log( xhr.responseText ); // <- xhr.responseText 로 결과를 가져올 수 있음
+              
+              console.log(">>>>>>>>>>>>> get logic in my server")
+              res.send(JSON.parse(xhr.responseText));     
 		  }
 	   }
     }
-    
     console.log('start request');
     searchPubTransPathAJAX(xhr, apiKey);
-    
-    console.log(">>>>>>>>>>>>> get logic in my server")
-    console.log(JSON.parse(xhr.responseText));
-    res.send(JSON.parse(xhr.responseText));  
 });
 
 // 서버 활성화 설정해 놓은 port 와 hostName 의 주소로 들어오는 클라이언트들을 감지한다.
