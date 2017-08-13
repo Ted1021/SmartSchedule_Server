085e757fbda835deccd98dd8c3a3fa4ae17a320a
Ted <kimtx2017@gmail.com>
2017-08-13 21:25:21 +0900

    Fix bug

--- express_demo/express_server.js
+++ express_demo/express_server.js
@@ -3,18 +3,20 @@ var app = express();
 var http = require('http');
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
-function searchPubTransPathAJAX(res, apiKey) {
+function searchPubTransPathAJAX(res, apiKey, sx, sy, ex, ey) {
     
     var xhr = new XMLHttpRequest();
     var url = 'https://api.odsay.com/api/searchPubTransPath?SX='+sx+'&SY='+sy+'&EX='+ex+'&EY='+ey+'&apiKey='+apiKey;
-	   
+
+    console.log(url);
+    
     xhr.open("GET", url, true);
     xhr.send();
     xhr.onreadystatechange = function() {
 
         if (xhr.readyState == 4 && xhr.status == 200) {
             console.log(">>>>>>>>> return from Odsay");
-            console.log( xhr.responseText ); // <- xhr.responseText 로 결과를 가져올 수 있음
+//            console.log( xhr.responseText ); // <- xhr.responseText 로 결과를 가져올 수 있음
             
             console.log(">>>>>>>>>>>>> get logic in my server")
             res.send(JSON.parse(xhr.responseText));     
@@ -27,10 +29,13 @@ app.get('/route', function(req, res){
     var sx = req.query.departX, sy = req.qeury.departY;
     var ex = req.qeury.destX, ey = req.query.destY;
     
+    console.log('departX = '+sx+' / departY = '+sy);
+    console.log('destX = '+ex+' / destY = '+ey);
+    
     var apiKey = 'bFNbGwjMxjtTaiMEroskKw';
     
     console.log('start request');
-    searchPubTransPathAJAX(res, apiKey);
+    searchPubTransPathAJAX(res, apiKey, sx, sy, ex, ey);
 });
 
 // 서버 활성화 설정해 놓은 port 와 hostName 의 주소로 들어오는 클라이언트들을 감지한다.
